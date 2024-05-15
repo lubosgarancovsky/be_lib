6a421c9e745c3ed7edc073de13c798836b3975e5
Ľuboš Garančovský <lubos.garancovsky@gmail.com>
2024-05-16 00:17:34 +0200

    paginated result

--- domain/src/main/java/io/github/lubosgarancovsky/domain/listing/PaginatedResult.java
+++ domain/src/main/java/io/github/lubosgarancovsky/domain/listing/PaginatedResult.java
@@ -1,4 +1,13 @@
 package io.github.lubosgarancovsky.domain.listing;
 
-public interface PaginatedResult {
+import java.util.List;
+
+public interface PaginatedResult<T extends PaginatedResultItem> {
+    List<T> items();
+
+    Integer page();
+
+    Integer pageSize();
+
+    Integer totalCount();
 }
--- domain/src/main/java/io/github/lubosgarancovsky/domain/listing/PaginatedResultItem.java
+++ domain/src/main/java/io/github/lubosgarancovsky/domain/listing/PaginatedResultItem.java
@@ -1,4 +1,7 @@
 package io.github.lubosgarancovsky.domain.listing;
 
+import java.util.List;
+
 public interface PaginatedResultItem {
+
 }
